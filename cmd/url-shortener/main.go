5a5b25563f9e138080866cfdb3039c9eb5eddbc8
gerneram <120254185+gerneram@users.noreply.github.com>
2025-07-30 17:23:07 +0300

    REST API сервис с использованием SQLite Логирования HTTP-роутинга и написания юнит тестов

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+	"golang.org/x/exp/slog"
+	"log"
+	"net/http"
+	"os"
+	"project/internal/config"
+	"project/internal/http-server/handlers/redirect"
+	"project/internal/http-server/handlers/url/save"
+	mwLogger "project/internal/http-server/middleware/logger"
+	slogpretty "project/internal/lib/logger/handlers/slogretty"
+	"project/internal/lib/logger/sl"
+	"project/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+	envDev   = "dev"
+)
+
+func main() {
+
+	if err := godotenv.Load("local.env"); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+	log.Error("error message")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("error init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	router.Route("/url", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+			cfg.HTTPServer.User: cfg.HTTPServer.Password,
+		}))
+		r.Post("/", save.New(log, storage))
+
+	})
+	router.Get("/{alias}", redirect.New(log, storage))
+	log.Info("starting server", slog.String("address", cfg.Address))
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("error starting server", sl.Err(err))
+	}
+
+	log.Error("stopping server", slog.String("address", cfg.Address))
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
